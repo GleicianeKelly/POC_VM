a38133c755ead1d4b772793254344a023e41c4b6
Araujo <gleiciane.de.araujo@accenture.com>
2023-11-28 10:56:02 -0300

    Alteracao no pom

--- src/main/java/teste/vm/demo/TesteApplication.java
+++ src/main/java/teste/vm/demo/TesteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SearaApplication {
+public class TesteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SearaApplication.class, args);
+		SpringApplication.run(TesteApplication.class, args);
 	}
 
 }
